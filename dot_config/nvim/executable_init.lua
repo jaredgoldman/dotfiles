dda3cf04594f71d19e56b6a2535589da71ae6b4a
jaredgoldman <jaredgoldmandev@gmail.com>
2023-04-15 12:59:24 -0400

    Add i3 and nvim configs

---
+++ dot_config/nvim/executable_init.lua
@@ -0,0 +1,12 @@
+-- Standard vim options
+require("algorandpa.plugins-setup")
+require("algorandpa.core.options")
+require("algorandpa.core.keymaps")
+require("algorandpa.core.colorscheme")
+require("algorandpa.plugins.comment")
+require("algorandpa.plugins.nvim-tree")
+require("algorandpa.plugins.lualine")
+require("algorandpa.plugins.telescope")
+require("algorandpa.plugins.cmp")
+require("algorandpa.lsp.lsp-setup")
+require("algorandpa.lsp.autopairs")
