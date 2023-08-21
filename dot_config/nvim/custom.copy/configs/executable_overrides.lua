2026e101d713652995c6ebc556bfa40170a7f079
jaredgoldman <jaredgoldmandev@gmail.com>
2023-08-20 22:17:45 -0400

    FIX: Add custom copy of custom nvchad config to root dir

---
+++ dot_config/nvim/custom.copy/configs/executable_overrides.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "css",
+    "javascript",
+    "html",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "php",
+  },
+  indent = {
+    enable = true,
+  },
+  highlight = {
+    enable = true,
+  },
+  autotag = {
+    enable = true,
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+    "intelephense",
+    "tailwindcss",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
