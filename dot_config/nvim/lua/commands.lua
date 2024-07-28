d4885c1841cc7481bffe207ae24b425d7706208c
jaredgoldman <jaredgoldmandev@gmail.com>
2024-07-27 20:32:35 -0400

    feat: add updated nvim config

--- dot_config/nvim/lua/commands.lua
+++ dot_config/nvim/lua/commands.lua
@@ -4,3 +4,16 @@ local api = vim.api
 api.nvim_create_user_command("ShowFilePath", function()
 	print(vim.fn.expand("%:p"))
 end, { bang = true })
+
+-- Toggle wrap
+vim.api.nvim_create_user_command("ToggleWrap", function()
+	if vim.wo.wrap then
+		vim.wo.wrap = false
+		print("Word wrap disabled")
+	else
+		vim.wo.wrap = true
+		vim.wo.linebreak = true
+		vim.wo.breakindent = true
+		print("Word wrap enabled")
+	end
+end, {})
