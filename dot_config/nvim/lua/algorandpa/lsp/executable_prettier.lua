0f21acb7683af5a8b48ed086c4e5561245ec3e86
jaredgoldman <jaredgoldmandev@gmail.com>
2024-07-27 21:19:12 -0400

    feat: remove old nvim config

--- dot_config/nvim/lua/algorandpa/lsp/executable_prettier.lua
+++
@@ -1,9 +0,0 @@
-require('vim-prettier').setup({
-    tab_width = 4,
-    single_quote = true,
-    bracket_spacing = false,
-    config_precedence = 'file-override',
-    write_mode = 'overwrite',
-    format_on_save = true,
-    eslint_enable_diagnostics = false
-})
