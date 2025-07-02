3ae882f5ef3e66c7ccf5849f826b0a0e410f1ca2
Jared Goldman <jaredgoldmandev@gmail.com>
2025-07-01 19:17:26 -0600

    feat: remove dot_git files

--- dot_config/nvim/lua/config/executable_mason.lua
+++ dot_config/nvim/lua/config/executable_mason.lua
@@ -1,30 +1,53 @@
 -- Mason
 require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
+
 -- A list of servers to automatically install if they're not already installed
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "ts_ls",
-    "jsonls",
-    "lua_ls",
-    "pyright",
-    "intelephense",
-  },
+	ensure_installed = {
+		"ts_ls",
+		"jsonls",
+		"lua_ls",
+		"pyright",
+		"intelephense",
+		-- New additions
+		"html",           -- HTML LSP
+		"bashls",         -- Bash LSP
+		"yamlls",         -- YAML LSP
+		"sqlls",          -- SQL LSP
+	},
 })
 
 require("mason-tool-installer").setup({
-  ensure_installed = {
-    "prettier", -- prettier formatter
-    "stylua",   -- lua formatter
-    "black",    -- python formatter
-    "autopep8", -- another python formatter
-    "prettierd", -- faster prettier
-  }
+	ensure_installed = {
+		"prettier",
+		"stylua",
+		"black",
+		"autopep8",
+		"prettierd",
+		-- New additions
+		-- JavaScript/TypeScript
+		"eslint_d",       -- Fast ESLint daemon
+		-- Python
+		"ruff",           -- Fast Python linter/formatter
+		"isort",          -- Python import sorter
+		"mypy",           -- Python type checker
+		-- PHP
+		"php-cs-fixer",   -- PHP formatter
+		"phpstan",        -- PHP static analyzer
+		-- Bash
+		"shellcheck",     -- Shell script linter
+		"shfmt",          -- Shell formatter
+		-- YAML
+		"yamllint",       -- YAML linter
+		-- SQL
+		"sqlfluff",       -- SQL linter and formatter
+	},
 })
